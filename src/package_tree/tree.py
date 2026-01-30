afa20e4073c757b8796d1d2e8438daeac8c17c35
Alain Delplanque <alaindelplanque@mailoo.org>
2026-01-30 22:05:45 +0100

    feat: add --paths option to show package installation paths

--- src/package_tree/tree.py
+++ src/package_tree/tree.py
@@ -34,60 +34,72 @@ class Style(dict):
         return ', '.join('%s="%s"' % x for x in self.items())
 
 
-def format_label(pkg):
-    lines = [
-        pkg.name,
-        '<font point-size="10">%s</font>' % pkg.locate_file(""),
-        '<font point-size="10">%s</font>' % pkg.version,
-    ]
-    return "<br/>".join(lines)
+class Graph:
 
+    def __init__(self, args):
+        self.args = args
+        self.styles = {}
 
-def get_graph():
-    """
-    Packages and dependencies in dot language.
+    def format_label(self, pkg):
+        lines = [pkg.name]
+        if self.args.paths:
+            lines.append('<font point-size="10">%s</font>' % pkg.locate_file(""))
+        lines.append(f'<font point-size="10">{pkg.version}</font>')
+        return "<br/>".join(lines)
 
-    Return:
-        List of lines
-    """
-    deps = set()
+    def get_style(self, pkg):
+        if self.args.paths:
+            site_package = pkg.locate_file("")
+            if site_package not in self.styles:
+                self.styles[site_package] = Style()
+            return self.styles[site_package]
+        else:
+            return Style(color="#a6cee3")
 
-    for pkg in distributions():
-        if pkg.requires:
-            deps.update(
-                (canonicalize_name(pkg.name), canonicalize_name(req.name))
-                for req in (Requirement(x) for x in pkg.requires)
-                if not req.marker or req.marker.evaluate()
+    def get_graph(self):
+        """
+        Packages and dependencies in dot language.
+
+        Return:
+            List of lines
+        """
+        deps = set()
+
+        for pkg in distributions():
+            if pkg.requires:
+                deps.update(
+                    (canonicalize_name(pkg.name), canonicalize_name(req.name))
+                    for req in (Requirement(x) for x in pkg.requires)
+                    if not req.marker or req.marker.evaluate()
+                )
+
+        result = []
+        result.append("rankdir=LR;")
+
+        for pkg in distributions():
+            result.append(
+                '"%s" [%s, label=<%s>]'
+                % (
+                    canonicalize_name(pkg.name),
+                    self.get_style(pkg),
+                    self.format_label(pkg),
+                )
             )
+        for relation in sorted(deps):
+            result.append('"%s" -> "%s";' % relation)
 
-    result = []
-    result.append("rankdir=LR;")
+        return result
 
-    styles = {}
-
-    for pkg in distributions():
-        site_package = pkg.locate_file("")
-        if site_package not in styles:
-            styles[site_package] = Style()
-        result.append('"%s" [%s, label=<%s>]' % (
-            canonicalize_name(pkg.name), styles[site_package], format_label(pkg))
-        )
-    for relation in sorted(deps):
-        result.append('"%s" -> "%s";' % relation)
-
-    return result
-
-
-def write_dot_file(stream):
-    """
-    Write full dot script to stream.
-    """
-    stream.write("digraph G {\n")
-    for l in get_graph():
-        stream.write("    ")
-        stream.write(l)
-        stream.write("\n")
-    stream.write("}\n")
+    def write_dot_file(self, stream):
+        """
+        Write full dot script to stream.
+        """
+        stream.write("digraph G {\n")
+        for l in self.get_graph():
+            stream.write("    ")
+            stream.write(l)
+            stream.write("\n")
+        stream.write("}\n")
 
 
 def build(script_file, outfile, fmt="png"):
@@ -99,15 +111,22 @@ def build(script_file, outfile, fmt="png"):
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="Dependencies of python packages")
+        description="Generate a dependency graph of installed Python packages."
+    )
+    parser.add_argument("filename", help="Output file path for the generated graph.")
     parser.add_argument(
-        'filename',
-        help="Fichier de sortie")
+        "-f",
+        "--format",
+        choices=["png", "svg", "dot"],
+        default="png",
+        help="Output file format (default: png).",
+    )
     parser.add_argument(
-        '-f', '--format',
-        choices=['png', 'svg', 'dot'],
-        default='png',
-        help='Output format')
+        "--paths",
+        action="store_true",
+        default=False,
+        help="Include the filesystem paths where each package is installed.",
+    )
     return parser.parse_args()
 
 
@@ -115,10 +134,10 @@ def main():
     args = parse_args()
     if args.format == 'dot':
         with open(args.filename, 'w') as outfile:
-            write_dot_file(outfile)
+            Graph(args).write_dot_file(outfile)
     else:
         with tempfile.TemporaryFile('w') as script_file, \
              open(args.filename, 'wb') as outfile:
-            write_dot_file(script_file)
+            Graph(args).write_dot_file(script_file)
             script_file.seek(0)
             build(script_file, outfile, args.format)
