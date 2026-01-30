ff41d2e1ae3ccf201d180ab755789a7f7a6960d6
adelplanque <alaindelplanque@mailoo.org>
2026-01-30 19:06:57 +0100

    fix: drop python2 support and pkg_resources

--- src/package_tree/tree.py
+++ src/package_tree/tree.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
-
 import argparse
-import os
-import pkg_resources
-import six
 import subprocess
 import tempfile
+from importlib.metadata import distributions
+
+from packaging.requirements import Requirement
+from packaging.utils import canonicalize_name
 
 
 class inf_loop(list):
@@ -20,7 +19,6 @@ class inf_loop(list):
                 yield x
 
 
-@six.python_2_unicode_compatible
 class Style(dict):
 
     _colors = iter(inf_loop('#a6cee3', '#b2df8a', '#fb9a99', '#fdbf6f',
@@ -28,7 +26,7 @@ class Style(dict):
                             '#e31a1c', '#ff7f00', '#6a3d9a', '#b15928'))
 
     def __init__(self, color=None):
-        self['color'] = color or six.next(self._colors)
+        self['color'] = color or next(self._colors)
         self['shape'] = 'box'
         self['style'] = 'filled'
 
@@ -36,29 +34,13 @@ class Style(dict):
         return ', '.join('%s="%s"' % x for x in self.items())
 
 
-def get_site_package(path):
-    folders = []
-    while True:
-        path, folder = os.path.split(path)
-        if folder != "":
-            folders.append(folder)
-        else:
-            break
-    folders.reverse()
-    try:
-        idx = folders.index('site-packages')
-        folders = folders[0:idx]
-    except ValueError:
-        pass
-    return '/' + '/'.join(folders)
-
-
 def format_label(pkg):
-    lines = [pkg.key]
-    lines.append('<font point-size="10">%s</font>'
-                 % get_site_package(pkg.location))
-    lines.append('<font point-size="10">%s</font>' % pkg.version)
-    return '<br/>'.join(lines)
+    lines = [
+        pkg.name,
+        '<font point-size="10">%s</font>' % pkg.locate_file(""),
+        '<font point-size="10">%s</font>' % pkg.version,
+    ]
+    return "<br/>".join(lines)
 
 
 def get_graph():
@@ -68,23 +50,27 @@ def get_graph():
     Return:
         List of lines
     """
-    pkgs = pkg_resources.working_set
     deps = set()
-    for pkg in pkgs:
-        for requirement in pkg.requires():
-            deps.add((pkg.key, requirement.key))
+
+    for pkg in distributions():
+        if pkg.requires:
+            deps.update(
+                (canonicalize_name(pkg.name), canonicalize_name(req.name))
+                for req in (Requirement(x) for x in pkg.requires)
+                if not req.marker or req.marker.evaluate()
+            )
 
     result = []
     result.append("rankdir=LR;")
 
     styles = {}
 
-    for pkg in sorted(pkgs, key=lambda x: x.key):
-        site_package = get_site_package(pkg.location)
+    for pkg in distributions():
+        site_package = pkg.locate_file("")
         if site_package not in styles:
             styles[site_package] = Style()
         result.append('"%s" [%s, label=<%s>]' % (
-            pkg.key, styles[site_package], format_label(pkg))
+            canonicalize_name(pkg.name), styles[site_package], format_label(pkg))
         )
     for relation in sorted(deps):
         result.append('"%s" -> "%s";' % relation)
